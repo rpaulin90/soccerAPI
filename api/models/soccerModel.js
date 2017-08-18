6b2e875025c2e9d55d09ef5549484eca883ec780
Ricardo Paulin <rpaulin@Ricardos-MacBook-Pro.local>
2017-08-17 20:07:07 -0500

    modified model

--- api/models/soccerModel.js
+++ api/models/soccerModel.js
@@ -20,31 +20,112 @@ var Schema = mongoose.Schema;
 // });
 
 
+// _links: {
+//     self: {
+//         href: {
+//
+//             type: String
+//
+//          }
+//     },
+//     competition: {
+//         href: {
+//
+//             type: String
+//
+//          }
+//     },
+//     homeTeam: {
+//         href: {
+//
+//             type: String
+//
+//          }
+//     },
+//     awayTeam: {
+//         href: {
+//
+//             type: String
+//
+//          }
+//     }
+// },
+// "date": "2017-08-26T14:00:00Z",
+//     "status": "TIMED",
+//     "matchday": 3,
+//     "homeTeamName": "Newcastle United FC",
+//     "awayTeamName": "West Ham United FC",
+//     "result": {
+//     "goalsHomeTeam": null,
+//         "goalsAwayTeam": null
+// },
+// odds: null
+
+
 var FixtureSchema = new Schema({
 
-    date: {
-        type: String
-    },
-    status: {
-        type: String
-    },
-    matchday: {
-        type: String
-    },
-    homeTeamName: {
-        type: String
-    },
-    awayTeamName: {
-        type: String
-    },
-    result: {
-        goalsHomeTeam: {
-            type: Number
-        },
-        goalsAwayTeam: {
-            type: Number
+    fixtures: [
+        {
+
+            _links: {
+                self: {
+                    href: {
+
+                        type: String
+
+                     }
+                },
+                competition: {
+                    href: {
+
+                        type: String
+
+                     }
+                },
+                homeTeam: {
+                    href: {
+
+                        type: String
+
+                     }
+                },
+                awayTeam: {
+                    href: {
+
+                        type: String
+
+                     }
+                }
+            },
+
+            date: {
+                type: String
+            },
+            status: {
+                type: String
+            },
+            matchday: {
+                type: String
+            },
+            homeTeamName: {
+                type: String
+            },
+            awayTeamName: {
+                type: String
+            },
+            result: {
+                goalsHomeTeam: {
+                    type: Number
+                },
+                goalsAwayTeam: {
+                    type: Number
+                }
+            },
+            odds: null
+
+
         }
-    }
+    ]
 
 
 
