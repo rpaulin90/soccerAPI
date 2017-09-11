474dd041dbc5435243a3fcbbbdbeb0be94d01ef0
Ricardo Paulin <rpaulin@ricardos-mbp.attlocal.net>
2017-09-11 17:57:52 -0500

    changed model to player stats

--- api/models/soccerModel.js
+++ api/models/soccerModel.js
@@ -62,71 +62,195 @@ var Schema = mongoose.Schema;
 // odds: null
 
 
+// var FixtureSchema = new Schema({
+//
+//     fixtures: [
+//         {
+//
+//             _links: {
+//                 self: {
+//                     href: {
+//
+//                         type: String
+//
+//                      }
+//                 },
+//                 competition: {
+//                     href: {
+//
+//                         type: String
+//
+//                      }
+//                 },
+//                 homeTeam: {
+//                     href: {
+//
+//                         type: String
+//
+//                      }
+//                 },
+//                 awayTeam: {
+//                     href: {
+//
+//                         type: String
+//
+//                      }
+//                 }
+//             },
+//
+//             date: {
+//                 type: String
+//             },
+//             status: {
+//                 type: String
+//             },
+//             matchday: {
+//                 type: String
+//             },
+//             homeTeamName: {
+//                 type: String
+//             },
+//             awayTeamName: {
+//                 type: String
+//             },
+//             result: {
+//                 goalsHomeTeam: {
+//                     type: Number
+//                 },
+//                 goalsAwayTeam: {
+//                     type: Number
+//                 }
+//             }
+//
+//         }
+//     ]
+//
+//
+//
+// });
+
 var FixtureSchema = new Schema({
 
-    fixtures: [
-        {
 
-            _links: {
-                self: {
-                    href: {
+    name: {
+        type: String
+    },
+    team: {
+        type: String
+    },
+    position: {
+        type: String
+    },
+    fixtures: {
+        type: Array
+    }
+    // gameweeks: [
+    //     {
+    //         gw1: {
+    //             minutesPlayed: {
+    //                 type: Number
+    //             },
+    //             goalsScored: {
+    //                 type: Number
+    //             },
+    //             goalsAssisted: {
+    //                 type: Number
+    //             },
+    //             goalsConceded: {
+    //                 type: Number
+    //             },
+    //             ownGoals: {
+    //                 type: Number
+    //             },
+    //             penaltiesSaved: {
+    //                 type: Number
+    //             },
+    //             penaltiesMissed: {
+    //                 type: Number
+    //             },
+    //             yellowCards: {
+    //                 type: Number
+    //             },
+    //             redCards: {
+    //                 type: Number
+    //             },
+    //             tackles: {
+    //                 type: Number
+    //             },
+    //             interceptions: {
+    //                 type: Number
+    //             },
+    //             recoveries: {
+    //                 type: Number
+    //             }
+    //         }
+    //     }
+    // ]
 
-                        type: String
-
-                     }
-                },
-                competition: {
-                    href: {
-
-                        type: String
-
-                     }
-                },
-                homeTeam: {
-                    href: {
-
-                        type: String
-
-                     }
-                },
-                awayTeam: {
-                    href: {
-
-                        type: String
-
-                     }
-                }
-            },
-
-            date: {
-                type: String
-            },
-            status: {
-                type: String
-            },
-            matchday: {
-                type: String
-            },
-            homeTeamName: {
-                type: String
-            },
-            awayTeamName: {
-                type: String
-            },
-            result: {
-                goalsHomeTeam: {
-                    type: Number
-                },
-                goalsAwayTeam: {
-                    type: Number
-                }
-            }
-
-        }
-    ]
+    // fixtures: [
+    //     {
+    //
+    //         _links: {
+    //             self: {
+    //                 href: {
+    //
+    //                     type: String
+    //
+    //                 }
+    //             },
+    //             competition: {
+    //                 href: {
+    //
+    //                     type: String
+    //
+    //                 }
+    //             },
+    //             homeTeam: {
+    //                 href: {
+    //
+    //                     type: String
+    //
+    //                 }
+    //             },
+    //             awayTeam: {
+    //                 href: {
+    //
+    //                     type: String
+    //
+    //                 }
+    //             }
+    //         },
+    //
+    //         date: {
+    //             type: String
+    //         },
+    //         status: {
+    //             type: String
+    //         },
+    //         matchday: {
+    //             type: String
+    //         },
+    //         homeTeamName: {
+    //             type: String
+    //         },
+    //         awayTeamName: {
+    //             type: String
+    //         },
+    //         result: {
+    //             goalsHomeTeam: {
+    //                 type: Number
+    //             },
+    //             goalsAwayTeam: {
+    //                 type: Number
+    //             }
+    //         }
+    //
+    //     }
+    // ]
 
 
 
 });
+
 
 module.exports = mongoose.model('Fixture', FixtureSchema);
